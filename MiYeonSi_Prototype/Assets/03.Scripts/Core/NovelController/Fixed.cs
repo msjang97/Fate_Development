3978349fc245c7656dcf988cb5941f2e8fe08bc7
Kimsol <ksol92789@gmail.com>
2021-07-12 16:08:55 +0900

    해상도 버그 및 검은네모 버그 수정
    .

--- MiYeonSi_Prototype/Assets/03.Scripts/Core/NovelController/Fixed.cs
+++ MiYeonSi_Prototype/Assets/03.Scripts/Core/NovelController/Fixed.cs
@@ -12,8 +12,8 @@ public class Fixed : MonoBehaviour
     /* 해상도 설정하는 함수 */
     public void SetResolution()
     {
-        int setWidth = 1920; // 사용자 설정 너비
-        int setHeight = 1080; // 사용자 설정 높이
+        int setWidth = 1080; // 사용자 설정 너비
+        int setHeight = 1920; // 사용자 설정 높이
 
         int deviceWidth = Screen.width; // 기기 너비 저장
         int deviceHeight = Screen.height; // 기기 높이 저장
