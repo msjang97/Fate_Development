b38096c7e57a67945d3658fb6359a3d0a5c7b566
jwnsqud <jwnsqud@gmail.com>
2021-07-13 15:08:45 +0900

    추가 미니게임 수정
    1. 끝부분 멈춤 처리
    2. 게이지에 따라 날라감
    3. 아직 멈춤후 처리하는중

---
+++ MiYeonSi_Prototype/Assets/03.Scripts/LSP_Minigame/HeartControll.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartControll : MonoBehaviour
+{
+    public bool stopHeart = false;// 하트 정지
+    public bool movestopHeart = false; // 가다가 멈춘 하트판단
+    public ShotputControll SPC;
+
+    
+
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Left" || col.gameObject.tag == "Up" || col.gameObject.tag == "Right")
+        {
+            Debug.Log("충돌");
+            stopHeart = true;
+        }
+
+    }
+
+    private void OnTriggerStay2D(Collider2D col)
+    {
+        if (movestopHeart)
+        {
+            if (col.gameObject.tag == "Best")
+            {
+               //ChoiceManager.P_instance.selectedNum = 4;
+               // LovePoint.instance.eunji_LovePoint += 5;
+                movestopHeart = false;
+
+                Debug.Log("Best");
+            }
+
+            else if (col.gameObject.tag == "Good")
+            {
+               // ChoiceManager.P_instance.selectedNum = 3;
+               // LovePoint.instance.eunji_LovePoint += 3;
+                movestopHeart = false;
+
+                Debug.Log("Good");
+
+            }
+
+            else if (col.gameObject.tag == "Normal")
+            {
+                //ChoiceManager.P_instance.selectedNum = 2;
+               // LovePoint.instance.eunji_LovePoint += 0;
+                movestopHeart = false;
+
+                Debug.Log("Normal");
+
+            }
+
+            else //if (col.gameObject.tag == "Worst")
+            {
+                //ChoiceManager.P_instance.selectedNum = 1;
+               // LovePoint.instance.eunji_LovePoint += -5;
+                movestopHeart = false;
+
+                Debug.Log("Worst");
+
+            }
+        }
+    }
+}
--- MiYeonSi_Prototype/Assets/03.Scripts/LSP_Minigame/ShotputControll.cs
+++ MiYeonSi_Prototype/Assets/03.Scripts/LSP_Minigame/ShotputControll.cs
@@ -6,20 +6,29 @@ using UnityEngine.UI;
 public class ShotputControll : MonoBehaviour
 {
     public GameObject powerBar; // 게이지바 
+    public GameObject choice_lowest; // 가장 점수낮은 범위 
     public float rz; // Z축 -60~ 60 제한
     public float _rz; // 임시 rz저장
     
     float mz = 100f; //속도
     public int count = 0; //버튼 카운트 0 : 방향조절 1: 세기조절
 
+    public float min; //Worst판 최저 y좌표
+    public float max; //Woest판 최고 y좌표
+    public float range;// Worst판 총 길이
+    public float pos_y; //현재 하트 y좌표
+
+
     private float upPower = 0; // 게이지 
-    private float _upPower = 0; // 게이지 
-    private float p_count = 10; // 게이지 속도
-    public Slider TimeSlider;
+    public float powerSpeed = 0; 
+    public float _powerSpeed = 0; 
+    public float p_count = 0; // 게이지 속도
+    public Slider powerSlider; // 총 게이지 수치
     private bool buttonDown;
 
     public GameObject heart; // 게이지바 
 
+    public HeartControll heartCon;//하트 컨트롤 스크립트
 
     private void Start()
     {
@@ -38,23 +47,28 @@ public class ShotputControll : MonoBehaviour
                 PowerCharging();
             else {
                 PowerStop();
-               // HeartMovement();
             }
         }
-        
+
+        if (heartCon.stopHeart)
+        {
+            powerSpeed = 0;
+            heartCon.stopHeart = false;
+        }
+        HeartStop();
     }
 
     void Init()
     {
-        TimeSlider.maxValue = 60; // 최대 60 슬라이더
+        powerSlider.maxValue = 100; // 최대 60 슬라이더
         rz = 60;
         _rz = 0;
         mz = 100;
         count = 0;
 
         upPower = 0;
-        _upPower = 0;
-        p_count = 30;
+        //powerSpeed = 0;
+        p_count = 70;
     }
 
     public void PointerDown()
@@ -89,7 +103,7 @@ public class ShotputControll : MonoBehaviour
     {
         count++;
         _rz = rz;
-        //if (count == 1) _upPower = upPower;
+        //if (count == 1) powerSpeed = upPower;
     }
 
     void PowerCharging() // 게이지채우기
@@ -97,43 +111,142 @@ public class ShotputControll : MonoBehaviour
         powerBar.SetActive(true);
 
         upPower += p_count * Time.deltaTime;
-        TimeSlider.value = upPower;
+        powerSlider.value = upPower;
 
-        if (TimeSlider.value == TimeSlider.maxValue || TimeSlider.value == TimeSlider.minValue)
+        if (powerSlider.value == powerSlider.maxValue || powerSlider.value == powerSlider.minValue)
             p_count = p_count * -1;
-        _upPower = upPower;
+        powerSpeed = upPower;
+        _powerSpeed = upPower;
     }
 
     void PowerStop() // 게이지 정지
     {
-        TimeSlider.value = _upPower;
+        powerSlider.value = _powerSpeed;
         HeartMovement();
-
+        //HeartStop();
     }
 
 
     void HeartMovement() //하트이동 함수
     {
-        if(count == 2)
+        //_powerSpeed = powerSpeed;
+        if (count == 2)
         heart.SetActive(true);
 
         heart.transform.rotation = Quaternion.Euler(0f, 0f, _rz);
 
-        //float mx = _upPower * Time.deltaTime;
+        //float mx = powerSpeed * Time.deltaTime;
 
-        if (heart.transform.rotation.z >= 0 && heart.transform.rotation.z <= 60)
+        if (_rz > 0 && _rz <= 60)
         {
-            heart.transform.Translate(Vector3.left * _upPower*10 * Time.deltaTime);
-            heart.transform.Translate(Vector3.up * _upPower * 30 * Time.deltaTime);
+            heart.transform.Translate(Vector3.left * powerSpeed*10 * Time.deltaTime);
+            heart.transform.Translate(Vector3.up * powerSpeed * 30 * Time.deltaTime);
 
         }
 
-        else if (heart.transform.rotation.z <= 0 && heart.transform.rotation.z >= -60)
+        else if (_rz < 0 && _rz >= -60)
         {
-            heart.transform.Translate(Vector3.right * _upPower * 10 * Time.deltaTime);
-            heart.transform.Translate(Vector3.up * _upPower * 30 * Time.deltaTime);
+            heart.transform.Translate(Vector3.right * powerSpeed * 10 * Time.deltaTime);
+            heart.transform.Translate(Vector3.up * powerSpeed * 30 * Time.deltaTime);
         }
 
+    }
+   
+   void HeartStop()
+    {
+        min = choice_lowest.transform.position.y- 525;
+        max = choice_lowest.transform.position.y + 525;
+        range = max - min;
+
+        pos_y = heart.transform.position.y;
+        
+       
+        if (min < heart.transform.position.y && max > heart.transform.position.y)
+        {
+            switch ((int)_powerSpeed % 100 / 10)
+            {
+                case 0:
+                    if (range / 10 * 0 + min == (int)heart.transform.position.y)
+                    {
+                        powerSpeed = 0;
+                        heartCon.movestopHeart = true;
+                    }
+                    break;
+                case 1:
+                    if (range / 10 * 1 + min == (int)heart.transform.position.y)
+                    {
+                        powerSpeed = 0;
+                        heartCon.movestopHeart = true;
+                    }
+                        break;
+                case 2:
+                    if (range / 10 * 2 + min == (int)heart.transform.position.y)
+                    {
+                        powerSpeed = 0;
+                        heartCon.movestopHeart = true;
+                    }
+                    break;
+                case 3:
+                    if (range / 10 * 3 + min == (int)heart.transform.position.y)
+                    {
+                        powerSpeed = 0;
+                        heartCon.movestopHeart = true;
+                    }
+                    break;
+                case 4:
+                    if (range / 10 * 4 + min == (int)heart.transform.position.y)
+                    {
+                        powerSpeed = 0;
+                        heartCon.movestopHeart = true;
+                    }
+                    break;
+                case 5:
+                    if (range / 10 * 5 + min == (int)heart.transform.position.y)
+                    {
+                        powerSpeed = 0;
+                        heartCon.movestopHeart = true;
+                    }
+                    break;
+                case 6:
+                    if (range / 10 * 6 + min == (int)heart.transform.position.y)
+                    {
+                        powerSpeed = 0;
+                        heartCon.movestopHeart = true;
+                    }
+                    break;
+                case 7:
+                    if (range / 10 * 7 + min == (int)heart.transform.position.y)
+                    {
+                        powerSpeed = 0;
+                        heartCon.movestopHeart = true;
+                    }
+                    break;
+                case 8:
+                    if (range / 10 * 8 + min == (int)heart.transform.position.y)
+                    {
+                        powerSpeed = 0;
+                        heartCon.movestopHeart = true;
+                    }
+                    break;
+                case 9:
+                    if (range / 10 * 9 + min == (int)heart.transform.position.y)
+                    {
+                        powerSpeed = 0;
+                        heartCon.movestopHeart = true;
+                    }
+                    break;
+                case 10:
+                    if (range / 10 * 10 + min == (int)heart.transform.position.y)
+
+                    {
+                        powerSpeed = 0;
+                        heartCon.movestopHeart = true;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 
 }
