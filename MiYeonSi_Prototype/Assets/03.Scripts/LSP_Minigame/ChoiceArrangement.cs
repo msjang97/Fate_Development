f539ecae3b50a0508e5f22255ecad7923f5852cc
jwnsqud <jwnsqud@gmail.com>
2021-08-25 15:34:23 +0900

    투포환미니게임 위치 수정
    1. worst good 위치 서로 랜덤하게 스왑되어서 나오게함

--- MiYeonSi_Prototype/Assets/03.Scripts/LSP_Minigame/ChoiceArrangement.cs
+++ MiYeonSi_Prototype/Assets/03.Scripts/LSP_Minigame/ChoiceArrangement.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class ChoiceArrangement : MonoBehaviour
 {
-    public GameObject best; //회색
-    public GameObject good; //노란색
+    public GameObject best; 
+    public GameObject good; 
+    public GameObject worst;
 
     public float bestMoveX;
     public float goodMoveX;
+    public float worstMoveX;
+
+    public float[] xxx = new float[2];
     // best - (325) (540)
     // good - (330) (750)
+    private void Awake()
+    {
+        xxx[0] = -356f; ;
+        xxx[1] = 242f;
+    }
     private void Start()
     {
         MovePosition();
@@ -19,11 +28,18 @@ public class ChoiceArrangement : MonoBehaviour
 
     void MovePosition()
     {
+       
         bestMoveX = Random.Range(-215, 215);
-        goodMoveX = Random.Range(-210, 210);
+        goodMoveX = Random.Range(0, 2);
+        if (goodMoveX == 0)
+            worstMoveX = 1;
+        else
+            worstMoveX = 0;
+        
 
         best.transform.localPosition = new Vector3(bestMoveX, 465f, 0f);
-        good.transform.localPosition = new Vector3(goodMoveX, -425f, 0f);
+        good.transform.localPosition = new Vector3(xxx[(int)goodMoveX], -401f, 0f);
+        worst.transform.localPosition = new Vector3(xxx[(int)worstMoveX], -531f, 0f);
 
     }
 
